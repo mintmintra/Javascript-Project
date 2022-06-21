6c7892813b14b1a9bfc1ea0c9de5f9841abcc5a9
mintmintra <mintracss019@gmail.com>
2022-06-21 23:56:17 +0700

    carousel

--- Carousel/start.js
+++ Carousel/start.js
@@ -1,4 +1,22 @@
 (() => {
-  
+  let currentIndex = 0;
+
+  function displayImage(imageElems, newIndex){
+    if(newIndex < 0){
+      newIndex = imageElems.length -1;
+    }
+    const newImage = imageElems[newIndex];
+    newImage.scrollIntoView({ behavior: 'smooth'})
+  }
+  function run(){
+    const imageElems = document.querySelectorAll('img');
+    const previousBtnElem = document.querySelector('.previous');
+    const nextBtnElem = document.querySelector('.next');
+
+    previousBtnElem.addEventListener('click', () => displayImage(imageElems, currentIndex -1))
+    nextBtnElem.addEventListener('click', () => displayImage(imageElems, currentIndex +1))
+  }
+
+  run();
 
 })();

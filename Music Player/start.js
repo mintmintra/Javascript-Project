10b2b560567b565c065dd3f4896abc769885a180
mintmintra <mintracss019@gmail.com>
2022-06-25 23:29:16 +0700

    Music Player

--- Music Player/start.js
+++ Music Player/start.js
@@ -1,4 +1,17 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const audioElem = document.querySelector('audio');
+  const playBtnElem = document.querySelector('.play');
+  const progressBarElem = document.querySelector('.progress-bar');
+  const startTimeElem = document.querySelector('.start-time');
+  const endTimeElem = document.querySelector('.end-time');
+
+  function onClick(){
+    audioElem.play();
+  }
+  
+  function run(){
+    playBtnElem.addEventListener('click', onClick);
+  }
+  run()
 
 })();

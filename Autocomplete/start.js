75f94f43bafb74a895c60b9d43e0efebe8f8951a
mintmintra <mintracss019@gmail.com>
2022-07-03 22:56:25 +0700

    Autocomplete

--- Autocomplete/start.js
+++ Autocomplete/start.js
@@ -1,3 +1,23 @@
 (() => {
- 
+    const carBrands = [
+        'BMW',
+        'Maserati',
+        'Mercedes Benz',
+        'Ferrari',
+        'Toyota',
+        'Honda',
+        'Hyundai'
+    ];
+
+    const searchElem = document.querySelector('.search');
+
+    function onInput(event){
+        const x = event.target.value;
+        console.log(x)
+    }
+
+    function run(){
+        searchElem.addEventListener('input', onInput);
+    }
+    run();
 })();

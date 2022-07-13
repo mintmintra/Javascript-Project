c6129e47e70114ffc35da26d4926e8fe60b32b13
mintmintra <mintracss019@gmail.com>
2022-07-13 22:23:16 +0700

    Watercolor Painting

--- Infinite Scroll Gallery/start.js
+++ Infinite Scroll Gallery/start.js
@@ -1,8 +1,10 @@
 (() => {
- const KEY = '';
-
- function disolayImages(){
-
+ const KEY = 'p3Sm0W3ROuxjmk5a9GuC-3WZHC3TDDMfLX4zuPN-slQ';
+ let page = 1;
+ 
+ async function disolayImages(){
+    const result =  await fetch(`https://api.unsplash.com/photos/?client_id=${KEY}&page=${page}`);
+    console.log(result)
  }
 
  function run(){

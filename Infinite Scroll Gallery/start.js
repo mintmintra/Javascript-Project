f3fe4296d7b8ac93f32d39dfa8ba5f99c2e4ddc2
mintmintra <mintracss019@gmail.com>
2022-08-01 23:11:56 +0700

    Infinite Scroll Gallery

--- Infinite Scroll Gallery/start.js
+++ Infinite Scroll Gallery/start.js
@@ -1,14 +1,47 @@
 (() => {
- const KEY = 'p3Sm0W3ROuxjmk5a9GuC-3WZHC3TDDMfLX4zuPN-slQ';
+ const KEY = '';
+ const loaderElem = document.querySelector('.loader');
  let page = 1;
+
+ function showLoader(){
+   loaderElem.classList.add('visible')
+ }
+
+ function hideLoader(){
+   loaderElem.classList.remove('visible');
+ }
  
- async function disolayImages(){
+ async function displayImages(){
+   showLoader();
+
     const result =  await fetch(`https://api.unsplash.com/photos/?client_id=${KEY}&page=${page}`);
-    console.log(result)
+    const images = await result.json();
+    
+    const galleryElem = document.querySelector('.gallery')
+    
+    images.forEach(image => {
+      const imgElem = document.createElement('img');
+      imgElem.src = image.urls.small;
+      
+      galleryElem.appendChild(imgElem)
+    });
+
+    hideLoader();
+
+    page += 1;
+ }
+
+ function onScroll(){
+   const { scrollTop, clientHeight, scrollHeight} = document.documentElement;
+
+   if(scrollTop + clientHeight >= scrollHeight - 10){
+      displayImages();
+   }
  }
 
  function run(){
-    disolayImages();
+    document.addEventListener('scroll', onScroll)
+    displayImages();
  }
  run();
 })();

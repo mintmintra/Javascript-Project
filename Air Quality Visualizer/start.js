da231dd4224e934d0611cf3539ffeef96ad58ab4
mintmintra <mintracss019@gmail.com>
2022-08-05 23:35:10 +0700

    Air Quality Visualizer

--- Air Quality Visualizer/start.js
+++ Air Quality Visualizer/start.js
@@ -1,4 +1,19 @@
 (() => {
-  
+    const KEY = '';
 
+    async function getAirQuality({ city, state, country}){
+        const response = await fetch(`https://api.airvisual.com/v2/city?city=${city}&state=${state}&country=${country}&key=${KEY}`)
+        console.log(response);
+    }
+
+
+    function run(){
+        const city = 'Sathon';
+        const state = 'Bangkok';
+        const country = 'Thailand';
+
+        getAirQuality({ city, state, country})
+    }
+
+    run();
 })();

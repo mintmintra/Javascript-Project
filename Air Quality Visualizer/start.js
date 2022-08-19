6c76915881b7e2ca1f7bd8b79d6c365608f5e48e
mintmintra <mintracss019@gmail.com>
2022-08-19 23:13:07 +0700

    Konami Code

--- Air Quality Visualizer/start.js
+++ Air Quality Visualizer/start.js
@@ -1,5 +1,5 @@
 (() => {
-    const KEY = '';
+    const KEY = '7597e3f9-1258-4d43-91a9-86a68bc71ed6';
 
     async function getAirQuality({ city, state, country}){
         const response = await fetch(`https://api.airvisual.com/v2/city?city=${city}&state=${state}&country=${country}&key=${KEY}`)

962171f10e3a8cf81bfc2391fa4d059e183fcfc5
mintmintra <mintracss019@gmail.com>
2022-10-08 21:56:27 +0700

    What is this

--- What is this/start.js
+++ What is this/start.js
@@ -1,6 +1,10 @@
 (() => {
 
   // 1. Lexical scope & Dynamic scope
+  function printName() {
+    console.log(this);
+    console.log(`My name is ${this.name}`);
+  }
   // 2. How to know what is "this"?
   //// 2.1 Invoker object
   //// 2.2 Global object (window, global)

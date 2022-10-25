378f7489ce3d95d5debae5164380e4f25c3256bd
mintmintra <mintracss019@gmail.com>
2022-10-25 20:53:17 +0700

    What is this

--- What is this/start.js
+++ What is this/start.js
@@ -3,10 +3,13 @@
   // 1. Lexical scope & Dynamic scope
   function printName() {
     console.log(this);
-    console.log(`My name is ${this.name}`);
   }
-  printName()
+  
   // 2. How to know what is "this"?
+  const varayut = { name: 'Varayut', printName };
+  const jane = { name: 'Jane', printName };
+  varayut.printName()
+  jane.printName()
   //// 2.1 Invoker object
   //// 2.2 Global object (window, global)
   //// 2.3 Constructor function

a3f6d76b7f95d859578fb0af490df1b5c0c3e7da
mintmintra <mintracss019@gmail.com>
2023-01-09 22:52:39 +0700

    ClockLightDarkMode

--- ClockLightDarkMode/app.js
+++ ClockLightDarkMode/app.js
@@ -1,4 +1,5 @@
 const timeEL = document.querySelector(".time")
+const btnToggle = document.querySelector(".toggle")
 
 function setTime(){
     const time = new Date()
@@ -9,5 +10,9 @@ function setTime(){
     ${minutes<10 ? `0${minutes}` : minutes}:
     ${seconds<10 ? `0${seconds}` : seconds}`
 }
+
+btnToggle.addEventListener("click",(e)=>{
+    
+})
 setTime()
 setInterval(setTime,1000)

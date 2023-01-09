20c4376007a664a8392f6e97a78768cdb975daf5
mintmintra <mintracss019@gmail.com>
2023-01-10 01:04:15 +0700

    ClockLightDarkMode

--- ClockLightDarkMode/app.js
+++ ClockLightDarkMode/app.js
@@ -12,7 +12,14 @@ function setTime(){
 }
 
 btnToggle.addEventListener("click",(e)=>{
-    
+    const html = document.querySelector('html')
+    if(html.classList.contains("dark")){
+        html.classList.remove("dark")
+        e.target.innerHTML="โหมดกลางคืน"
+    }else{
+        html.classList.add("dark")
+        e.target.innerHTML="โหมดกลางวัน"
+    }
 })
 setTime()
 setInterval(setTime,1000)

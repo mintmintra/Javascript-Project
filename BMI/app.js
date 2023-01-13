35ea14c988c5b8866192d66563e1fd9e2e42c78b
mintmintra <mintracss019@gmail.com>
2023-01-14 00:55:51 +0700

    BMI

--- BMI/app.js
+++ BMI/app.js
@@ -6,5 +6,11 @@ btn.addEventListener("click",(e)=>{
     let weight = document.getElementById("weight").value
     let height = document.getElementById("height").value
     
-    
+    if(weight === "" || isNaN(weight)){
+        return result.innerHTML = "กรุณาป้อนน้ำหนักของคุณ"
+    }else if(height === "" || isNaN(height)){
+        return result.innerHTML = "กรุณาป้อนส่วนสูง"
+    }else{
+        
+    }
 })

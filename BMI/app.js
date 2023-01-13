95b27c40c8c2ba0c9c1658059023f79a45874f53
mintmintra <mintracss019@gmail.com>
2023-01-13 23:48:06 +0700

    BMI

---
+++ BMI/app.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".btn")
+const result = document.querySelector(".result")
+
+btn.addEventListener("click",(e)=>{
+    e.preventDefault()
+    let weight = document.getElementById("weight").value
+    let height = document.getElementById("height").value
+    
+    
+})
